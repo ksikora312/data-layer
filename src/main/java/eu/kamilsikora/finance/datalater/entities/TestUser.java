e87d382b3b8861fa93a108ecc030d93f4ebe290a
Kamil <ksikora312@gmail.com>
2021-06-12 22:28:24 +0200

    init commit

---
+++ src/main/java/eu/kamilsikora/finance/datalater/entities/TestUser.java
@@ -0,0 +1,10 @@
+package eu.kamilsikora.finance.datalater.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class TestUser {
+    @Id
+    private Long id;
+}
